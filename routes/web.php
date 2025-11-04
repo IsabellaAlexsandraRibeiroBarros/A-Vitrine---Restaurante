f9a447d5b60c6479e49bfa5e3fc94088a562c662
Theo <theotino.2435@gmail.com>
2025-11-04 18:46:41 -0300

    routes e views criados

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/paginainicial', function () {
+    return view('paginainicial');
+});
+
+Route::get('/cardapio', function () {
+    return view('cardapio');
+});
+
+Route::get('/clientes', function () {
+    return view('clientes');
+});
+
+Route::get('/estoque', function () {
+    return view('estoque');
+});
+
+Route::get('/funcionarios', function () {
+    return view('funcionarios');
+});
+
+Route::get('/ingredientes', function () {
+    return view('ingredientes');
+});
+
+Route::get('mesas', function () {
+    return view('mesas');
+});
+
+Route::get('pedidos', function () {
+    return view('pedidos');
+});
